f6e7f3aa35dd1898e86391bc68fa6e03b1802b56
Willkell Boler <wmcboler@gmail.com>
2021-02-05 00:50:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+print('Welcome to my first game!')
+player_name = input("What is your name? ")
+print("Hey {name}!".format(name = player_name))
+player_age = input("Before we get started, what is your age? ")
+
+if int(player_age) > 12:
+  print("Awesome! Since you are {age}, you are old enough to play!".format(age = player_age))
+  ready = input("Are you ready to play?(yes or no?) ")
+  if ready == "yes":
+    print("Great! This is your first choice! You are on a random path and now have two ways to go.")
+    first_choice = input("Left or Right ?")
+
+
+else:
+  print("Sorry! Since you are {age}, you are NOT old enough to play!".format(age = player_age))
+  print("~~GAME OVER~~")
