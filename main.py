27aa391c6dc414c809e3fb546bd7251352d1270c
Willkell Boler <wmcboler@gmail.com>
2021-02-06 05:42:08 +0000

    Added more story onto the left side path choice. Also changed how I was adding in new lines. decided to put "\n" at beginning of each line, to make it easier moving forward and to make it more concise.

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 print('Welcome to my first game!\n')
 player_name = input("What is your name? ")
 print("Hey {name}!\n".format(name = player_name.title()))
-player_age = input("Before we get started, what is your age? ")
+player_age = input("\nBefore we get started, what is your age? ")
 
-if int(player_age) > 12:
-  print("Awesome! Since you are {age}, you are old enough to play!\n".format(age = player_age))
-  ready = input("Are you ready to play?(yes or no?) ")
+if int(player_age) > 5:
+  print("Awesome! Since you are {age}, you are old enough to play!".format(age = player_age))
+  ready = input("\nAre you ready to play?(yes or no?) ")
   if ready == "yes":
-    print("Great! Here is your first choice! You are on a random path and now have two ways to go.\n")
-    choice_1 = input("Left or Right ? ")
+    print("\nGreat! Here is your first choice! You are on a random path and now have two ways to go.")
+    choice_1 = input("\nLeft or Right? ")
+    #left side of path 
     if choice_1 == "left":
-        print("\nYou travel down the left path and eventually you arrive at a brickhouse. This brickhouse has a dock with a boat attached to it to go accross the lake. Do you want to take the boat or go in the house?\n")
-        choice_1 = input("boat or house? ")
+        print("\nYou travel down the left path and eventually you arrive at a brickhouse. This brickhouse has a dock with a boat attached to it to go accross the lake. Do you want to take the boat or go in the house?")
+        choice_1 = input("\nboat or house? ")
         if choice_1 == "boat":
-          print()
-          choice_1 = input()
+          print("\nYou jump inside the row boat that was tied to the dock. As you start to row the boat, you hear it creak as you slowly cross the lake. You start to get tired while rowing the boat while halfway on the lake. Do you want to stop or keep rowing?")
+          choice_1 = input("\nstop or row? ")
 
         else:
-          print()
-          choice_2 = input()  
-        
+          print("\nYou go to the front of the house and try to open the door. Apparently it is locked. Do you want to attempt to look for a key or kick the door in?")
+          choice_2 = input("\nsearch or kick?")  
+    #Right side of path     
     else:
-      print("\nYou travel down the right path and you go deeper into a forest. Threes start to get denser and you start to lose your way. Eventually you arrive at an old shack. Do you want to enter the shack or try to find a way out of the woods?\n")
-      choice_2 = input("shack or woods? ")
+      print("\nYou travel down the right path and you go deeper into a forest. Threes start to get denser and you start to lose your way. Eventually you arrive at an old shack. Do you want to enter the shack or try to find a way out of the woods?")
+      choice_2 = input("\nshack or woods? ")
       if choice_2 == "shack":
         print()
         choice_1 = input()
@@ -31,9 +32,9 @@ if int(player_age) > 12:
         print()
         choice_2 = input()
   else:
-    print("When you are ready just restart the game and be ready to play!")    
+    print("\nWhen you are ready just restart the game and be ready to play!")    
 
 
 else:
-  print("Sorry! Since you are {age}, you are NOT old enough to play!".format(age = player_age))
-  print("~~GAME OVER~~")
+  print("\nSorry! Since you are {age}, you are NOT old enough to play!".format(age = player_age))
+  print("\n~~GAME OVER~~")
